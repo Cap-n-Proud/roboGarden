7572fd3a6419c1bda5570e744b33f196825402de
Cap-n-Proud <kingsta@protonmail.com>
2021-01-20 22:35:53 +0000

    Add files via upload

---
+++ blueprints/status/status.py
@@ -0,0 +1,29 @@
+from blueprints.api import getPlantsDB, getStatus
+from flask import Blueprint
+from flask import current_app as app
+from flask import render_template
+
+# status = getStatus()
+# currentProgram = getCurrentProgram()
+# programs = getPrograms()
+
+# Blueprint Configuration
+status_bp = Blueprint(
+    "status_bp", __name__, template_folder="templates", static_folder="static"
+)
+
+
+@status_bp.route("/status", methods=["POST", "GET"])
+def status():
+    """Status page."""
+    plantsDB = getPlantsDB(app)
+    status = getStatus(app)
+    # print(config.Config.ASSETS_FOLDER)
+    return render_template(
+        "index.jinja2",
+        title="Status",
+        subtitle="Demonstration of Flask blueprints in action.",
+        template="status-template",
+        plantsDB=plantsDB,
+        status=status,
+    )
