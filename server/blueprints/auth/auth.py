18b94cc1f31c5600758b3ca0fa46bf864df08b7a
Ca--rod <kingsta@protonmail.com>
2021-04-11 23:17:36 +0200

    U

--- server/blueprints/auth/auth.py
+++ server/blueprints/auth/auth.py
@@ -43,7 +43,7 @@ def login_post():
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
-    return redirect(url_for("telemetry_bp.telemetry"))
+    return redirect(url_for("control_bp.control"))
 
 
 @auth_bp.route("/logout")
